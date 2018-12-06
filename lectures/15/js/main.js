eedf3f2113fcc4a25be11b20a8bb1ddbd65e963e
mustaqeem7 <mustaqeem71110@ucp.edu.pk>
2018-12-06 13:50:35 +0500

    Mock Quiz Done L1F16BSCS0305

--- lectures/15/js/main.js
+++ lectures/15/js/main.js
@@ -1,81 +1,88 @@
-var savingsAccount = {
-    title: 'Muhammad Ali',
-    balance: 962155,
-    currency: 'PKRS',
-    IBAN: 'PKN1234654321',
-    deposit: function addMoney(e,amount) {
-        if(e.keyCode === 13) {
-            if (!isNaN(amount) && parseInt(amount) > 0) {
-                savingsAccount.balance += parseInt(amount);
-                document.getElementById('deposit').value = '';
-                loadAccount();
-                savingsAccount.printTransactions('Credit', amount);
-            }else {
-                document.getElementById("deposit-msg").style.color = "red";
-                document.getElementById("deposit-msg").innerText = "Enter Valid Amount";
-            }
-        }
-    },
-    withdraw: function removeMoney(e,amount) {
-        if(e.keyCode === 13) {
-            if (!isNaN(amount) && parseInt(amount) > 0) {
-                var verifyBalance = savingsAccount.balance - parseInt(amount);
-                if(verifyBalance >= 0) {
-                    savingsAccount.balance -= parseInt(amount);
-                    document.getElementById('withdraw').value = '';
-                    loadAccount();
-                    savingsAccount.printTransactions('Debit', amount);
-                }
-                else {
-                    document.getElementById("withdraw-msg").style.color = "orange";
-                    document.getElementById("withdraw-msg").innerText = "Don't have sufficient amount in account ";
-                }
-            }else {
-                document.getElementById("withdraw-msg").style.color = "red";
-                document.getElementById("withdraw-msg").innerText = "Enter Valid Amount";
-            }
-        }
-    },
-    printTransactions: function print(tType, amount){
-        var table = document.getElementById("transaction-table");
-        var row = document.createElement("tr");
-        var dateTd = document.createElement("td");
-        var d = new Date();
-        d = month[d.getMonth()] + ' '+d.getDate() +', '+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
-        dateTd.innerHTML = d;
-        var typeTd = document.createElement("td");
-        typeTd.innerHTML = tType;
-        var amountTd = document.createElement("td");
-        amountTd.innerHTML = amount;
-        row.appendChild(dateTd);
-        row.appendChild(typeTd);
-        row.appendChild(amountTd);
-        table.appendChild(row);
-    }
-};
+let details = {
+    balance : 0,
+    title : "Muhammad Mustaqeem",
+    currency : "PKRS",
+    IBAN : "IBAN1234567890"};
 
-loadAccount();
-function loadAccount() {
-    var title = document.getElementById('title');
-    var balance = document.getElementById('balance');
-    var currency = document.getElementById('currency');
-    var IBAN = document.getElementById('IBAN');
-    title.innerHTML = savingsAccount.title;
-    balance.innerHTML = savingsAccount.balance;
-    currency.innerHTML = savingsAccount.currency;
-    IBAN.innerHTML = savingsAccount.IBAN;
+function SetTitle(){
+    var Acctitle = document.getElementById('title');
+    Acctitle.innerText = details.title;
+}
+function SetBalance(){
+    var AccBalanace = document.getElementById('balance');
+    AccBalanace.innerText = " ";
+    AccBalanace.innerText = details.balance;
+}
+function SetCurrency(){
+    var AccCurrency = document.getElementById('currency');
+    AccCurrency.innerText = details.currency;
+}
+function SetIBAN(){
+    var AccIBAN = document.getElementById('IBAN');
+    AccIBAN.innerText = details.IBAN;
 }
 
-var month = [];
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
+function SetDeposit(event) {
+    var InputDeposit = document.getElementById('deposit');
+    var Message = document.getElementById('deposit-msg');
+    Message.innerHTML = "";
+    if (event.which === 13 || event.key === "Enter") {
+        if (!isNaN(InputDeposit.value))
+        {
+            details.balance += parseInt(InputDeposit.value,10);
+            var AccBalanace = document.getElementById('balance');
+            SetBalance();
+            var table = document.getElementById('transaction-table');
+            table.innerHTML += '<tr>'+
+                '<td align="left" valign="middle" scope="col">'+ new Date() +'</td>\n' +
+                '<td align="left" valign="middle" scope="col">Deposit</td>\n' +
+                '<td align="left" valign="middle" scope="col">' + parseInt(InputDeposit.value,10) + '</td>\n' +
+                '</tr>';
+            InputDeposit.value ='';
+        }
+        else {
+            var message = "Enter Valid Amount";
+            var fMessage = message.fontcolor("red");
+            Message.innerHTML += fMessage;
+
+        }
+    }
+}
+function GetWithdraw(event) {
+    var InputWithdraw = document.getElementById('withdraw');
+    var Message = document.getElementById('withdraw-msg');
+    Message.innerHTML = "";
+    if (event.which === 13 || event.key === "Enter") {
+        if (!isNaN(InputWithdraw.value))
+        {
+            if (parseInt(InputWithdraw.value, 10) <= details.balance)
+            {
+                details.balance -= parseInt(InputWithdraw.value, 10);
+                var AccBalanace = document.getElementById('balance');
+                SetBalance();
+                var table = document.getElementById('transaction-table');
+                table.innerHTML += '<tr>'+
+                                   '<td align="left" valign="middle" scope="col">'+ new Date() +'</td>\n' +
+                                   '<td align="left" valign="middle" scope="col">Withdraw</td>\n' +
+                                   '<td align="left" valign="middle" scope="col">' + parseInt(InputWithdraw.value,10) + '</td>\n' +
+                                   '</tr>';
+                InputWithdraw.value='';
+            }
+            else
+            {
+                var message = "Don't Have Sufficient Amount In Account";
+                var finalMessage = message.fontcolor("orange");
+                Message.innerHTML += finalMessage;
+            }
+        }
+        else {
+            var message = "Enter Valid Amount";
+            var finalMessage = message.fontcolor("red");
+            Message.innerHTML += finalMessage;
+        }
+    }
+}
+SetTitle();
+SetBalance();
+SetCurrency();
+SetIBAN();
